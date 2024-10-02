34db8f3e8bed63594fc5c0554935b28269c20b04
Adrianna Pawlak <adrianna_p26@outlook.com>
2024-10-02 14:03:21 +0000

    added different types of flow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,44 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3 ,5);
+console.log(sum);
+
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3 ,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => (a + b);
+const addTwoNumbers2 = (a, b) => a + b;
+
+let sum2 = addTwoNumbers(2, 2);
+console.log(sum2);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
+
+const sayHello = () => console.log('hello!');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        Hello!
+    </p>`
+)
+console.log(returnMultipleLines());
